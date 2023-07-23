1c5c6d5c8212c52d7a5ba566211936521c011eb7
Lama Houssari <digitalxgurus@gmail.com>
2023-07-23 14:34:55 +0300

    got rid of camelCasing - cleaned up the code

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 import myModule from './myModule.cjs';
 import mongoose from 'mongoose';
-import camelcase from 'camelcase';
 
 main('').catch(err => console.log(err));
-
 const itemSchema = new mongoose.Schema({
     name: String
 })
@@ -17,39 +15,30 @@ const itemSchema = new mongoose.Schema({
 const lists = [
     {
         name: 'main',
-        camelName: 'main',
         model: mongoose.model('item', itemSchema)
     }
 ]
-let listTitle;
 async function main(func, listName, id) {
     await mongoose.connect('mongodb://127.0.0.1:27017/toDoListDB');
 
     if(func === 'get'){
         let model;
-        let found = false;
         lists.forEach((list)=>{
-            if(list.camelName === camelcase(listName)){
-                model = list.model
-                found = true;
-                listTitle = list.name;
-            }
+            if(list.name === listName){model = list.model}
         })
-        if(!found){
+        if(!model){
             lists.push({
                 name: listName,
-                camelName: camelcase(listName),
-                model: mongoose.model(`${camelcase(listName)}item`, itemSchema)
+                model: mongoose.model(`${listName}item`, itemSchema)
             })
             model = lists[lists.length - 1].model
-            listTitle = listName
         }
         return await model.find()
     }
     if(func === 'dl'){
         let model;
         lists.forEach((list)=>{
-            if(list.camelName === listName){
+            if(list.name === listName){
                 model = list.model
             }
         })
@@ -61,9 +50,8 @@ async function main(func, listName, id) {
 app.get('/', function(req, res){
     let items = main("get" ,"main")
     items.then(function(result) {
-        res.render('list' , {listTitle: 'main', camelName: 'main', items: result, date: myModule.getDate()})
+        res.render('list' , {listTitle: 'main', items: result, date: myModule.getDate()})
     })
-    
 })
 app.get('/about', function(req, res){
     res.render('about')
@@ -73,42 +61,37 @@ app.get('/:pageName', function(req, res){
     if(pageName !== 'about'){
         let items = main("get", pageName)
         items.then(function(result) {
-            res.render('list' , {listTitle: listTitle, camelName: camelcase(pageName), items: result, date: myModule.getDate()})
+            res.render('list' , {listTitle: pageName, items: result, date: myModule.getDate()})
         })
     }
 })
 
 //post requests
+// add list item
 app.post('/', function(req, res){
-    let camelListName = req.body.list
+    let referer = req.get('referer').split('/')
+    referer = referer[referer.length - 1].replaceAll('%20', ' ')
+    if(!referer){referer = 'main'}
     let newItem = req.body.newItem
-    let Model
     lists.forEach((list)=>{
-        if(list.camelName === camelListName){
-            Model = list.model
+        if(list.name === referer){
+            let item = new list.model ({name: newItem})
+            item.save()
         }
     })
-    let item = new Model ({
-        name: newItem
-    })
-    item.save()
-    res.redirect(`/${camelListName}`)
+    res.redirect(`/${referer}`)
 })
-
+// delete list item
 app.post('/delete', function(req, res){
     let id = req.body.id
-    let camelListName = req.body.list
-    main('dl', camelListName, id)
-    let listName;
-    lists.forEach((list)=>{
-        if(list.camelName === camelListName){
-            listName = list.name
-        }
-    })
-    res.redirect(`/${listName}`)
+    let referer = req.get('referer').split('/')
+    referer = referer[referer.length - 1].replaceAll('%20' , ' ')
+    if(!referer){referer = 'main'}
+    main('dl', referer, id)
+    res.redirect(`/${referer}`)
 })
-
-app.post('/new', function(req, res){
+// navigate lists
+app.post('/nav', function(req, res){
     let newListName = req.body.newList;
     if(newListName !== 'about'){
         res.redirect(`/${newListName}`)
